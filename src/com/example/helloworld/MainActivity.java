1bcb35d8fc358dd10cc901f53f027588fa7ba0e0
vaibhav puri <vaibhav@taggito.com>
2013-05-02 12:58:11 +0200

    First Commit

---
+++ src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import org.apache.cordova.*;
+import android.os.Bundle;
+
+
+public class MainActivity extends DroidGap {
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+
+    
+    
+}
